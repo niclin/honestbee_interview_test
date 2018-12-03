ac5ec3a7a27a954d858bb3f4bf065ed53c440a17
niclin <bboyceo@hotmail.com>
2018-12-04 01:09:47 +0800

    Add basic model for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 
+  validates :title, presence: true
+  validates :price, presence: true
+
   def discount_price
     (price * 0.8).round(2)
   end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
-
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a title" do
+    product = Product.new(title: nil)
+
+    product.valid?
+
+    expect(product.errors[:title]).to include("can't be blank")
+  end
+
+  it "is invalid without a price" do
+    product = Product.new(price: nil)
+
+    product.valid?
+
+    expect(product.errors[:price]).to include("can't be blank")
+  end
 end
