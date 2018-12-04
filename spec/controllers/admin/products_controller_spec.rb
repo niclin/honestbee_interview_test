329fd88635b46586e9fdc5bd1e136f3432a3be86
niclin <bboyceo@hotmail.com>
2018-12-04 22:21:07 +0800

    Add admin products controller basic test

--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -1,9 +1,37 @@
 RSpec.describe Admin::ProductsController, type: :controller do
-  context "when user is not login" do
-    it "redirect_to new_user_session_path" do
-      get :new
+  describe "GET index" do
+    context "when user is admin" do
+      let(:user) { create(:user, is_admin: true) }
+      let(:product1) { create(:product) }
+      let(:product2) { create(:product) }
 
-      expect(response).to redirect_to new_user_session_path
+      before do
+        sign_in user
+
+        get :index
+      end
+
+      it "assigns @products" do
+        expect(assigns[:products]).to eq([product1, product2])
+      end
+
+      it "render template" do
+        expect(response).to render_template("index")
+      end
+    end
+
+    context "when user is not admin" do
+      let(:user) { create(:user, is_admin: false) }
+
+      before do
+        sign_in user
+
+        get :index
+      end
+
+      it "redirect_to root_path" do
+        expect(response).to redirect_to root_path
+      end
     end
   end
 end
