36531fb0f714325dc07401ab5bfcfd778ba150e5
niclin <bboyceo@hotmail.com>
2018-12-04 15:05:46 +0800

    Edi travis.yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle exec rake db:create db:migrate db:seed
+# If database exists, migrate. Otherweise create and seed
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup db:seed
 
 exec bundle exec "$@"
