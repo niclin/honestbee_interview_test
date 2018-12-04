da1821da48373905c2fc699c14e315be48f8726e
niclin <bboyceo@hotmail.com>
2018-12-04 22:36:37 +0800

    Add post create test for admin/products controller

--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -34,4 +34,76 @@ RSpec.describe Admin::ProductsController, type: :controller do
       end
     end
   end
+
+  describe "GET new" do
+    context "when user is admin" do
+      let(:user) { create(:user, is_admin: true) }
+      let(:product) { build(:product) }
+
+      before do
+        sign_in user
+
+        get :new
+      end
+
+      it "assigns @product" do
+        expect(assigns[:product]).to be_a_new(Product)
+      end
+
+      it "render template" do
+        expect(response).to render_template("new")
+      end
+    end
+
+    context "when user is not admin" do
+      let(:user) { create(:user, is_admin: false) }
+
+      before do
+        sign_in user
+
+        get :new
+      end
+
+      it "redirect_to root_path" do
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "POST create" do
+    context "when user is admin" do
+      let(:user) { create(:user, is_admin: true) }
+      let(:product) { build(:product) }
+
+      before do
+        sign_in user
+      end
+
+      it "doesn't create a record" do
+        expect do
+          post :create, params: { product: { title: nil, price: nil } }
+        end.to change { Product.count }.by(0)
+      end
+
+      it "create a new produce record" do
+        expect do
+          post :create, params: { product: attributes_for(:product) }
+        end.to change { Product.count }.by(1)
+      end
+    end
+
+    context "when user is not admin" do
+      let(:user) { create(:user, is_admin: false) }
+
+      before do
+        sign_in user
+
+        post :create
+      end
+
+      it "redirect_to root_path" do
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
